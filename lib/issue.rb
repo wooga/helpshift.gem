dc571e8e95d836f4d91e46a55c37c80cbe493b6e
stephan lindauer <stephan.lindauer@gmail.com>
2015-10-07 15:17:10 +0200

    found issues with api regarding "tags" and "keys" -> left them out for now

--- lib/issue.rb
+++ lib/issue.rb
@@ -13,15 +13,18 @@ module HelpshiftGem
           "message-body"  => message_body,
           "app-id"        => app_id,
           "platform-type" => platform_type,
-          "tags"          => tags,
-          "meta"          => meta
-        }.reject!{|key,value| value.nil?}
+          # These to keys cause the API to return "400 Bad Request", so we leave them out for now:
+          # "tags"          => tags,
+          # "meta"          => meta
+        }
+        params.each {|key,value| params.delete(key) if value.nil? }
 
         RestClient.post request_uri, params, {:Authorization => "Basic #{Base64.encode64(HelpshiftGem.configuration.api_key)}"}
       end
     end
 
     def is_valid?
+      (@platform_type.nil? || ["ios", "android", "web"].include?(@platform_type)) &&
       (@tags.nil? || @tags.is_a?(Array)) &&
       (@meta.nil? || @meta.is_a?(Hash)) &&
       !@email.nil? && !@title.nil? && !@message_body.nil? && !@app_id.nil?
