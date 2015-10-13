3cb2477af08088b4dfc788483f2593c3b8f68d4d
stephan lindauer <stephan.lindauer@gmail.com>
2015-10-13 13:42:13 +0200

    fixed tests

--- test/unit/test_app.rb
+++ test/unit/test_app.rb
@@ -1,18 +1,31 @@
 require File.dirname(File.expand_path(__FILE__)) + '/../test_helper.rb'
 
 class AppTest < Minitest::Test
-  context "app" do
     def setup
       Helpshift.configure do |config|
-        config.customer_domain = "foobar"
         config.api_key = "foobaz"
+        config.customer_domain = "foobar"
+        config.base_domain = "helpshift.com"
       end
     end
 
+  context "app" do
     should 'fetch list of all apps' do
-      FakeWeb.register_uri(:get, "https://api.#{Helpshift.configuration.base_domain}/v1/#{Helpshift.configuration.customer_domain}/apps/",
-                           :body => '[{"platform_ids":["blabla_platform_1337-123","blabla_platform_123-456"],"publish_id":"13","title":"Blabla Game 1"},'+
-                                    '{"platform_ids":["blabla_platform_007-008","blabla_platform_000-111"],"publish_id":"42","title":"Blabla Game 2"}]' )
+
+      fake_response_array = Array.new(2)
+      fake_item_one = {"platform_ids" => ["blabla_platform_1337-123","blabla_platform_123-456"],
+                       "publish_id" => "13",
+                       "title" => "Blabla Game 1"}
+      fake_item_two = {"platform_ids" => ["blabla_platform_007-008","blabla_platform_000-111"],
+                       "publish_id" => "42",
+                       "title" => "Blabla Game 2"}
+
+      fake_response_array[0] = fake_item_one
+      fake_response_array[1] = fake_item_two
+
+      FakeWeb.allow_net_connect = false
+      FakeWeb.register_uri(:get, "https://#{Helpshift.configuration.api_key}@api.#{Helpshift.configuration.base_domain}/v1/#{Helpshift.configuration.customer_domain}/apps/",
+                           :body => fake_response_array.to_json )
 
       apps_array = Helpshift::App.all
       first_item_from_array = apps_array[0]
@@ -24,14 +37,16 @@ class AppTest < Minitest::Test
     end
 
     should 'fetch one specific app by id' do
-      app_publish_id = 1337
-      FakeWeb.register_uri(:get, "https://api.#{Helpshift.configuration.base_domain}/v1/#{Helpshift.configuration.customer_domain}/apps/#{app_publish_id}",
-                           :body => '{"platform_ids":["bla_platform_987654321-123456789"],'+
-                                      '"updated_at":1000000000000,'+
-                                      '"created_at":1000000000000,'+
-                                      '"title":"Game Name",'+
-                                      '"id":"moew_app_123456789-987654321",'+
-                                      '"publish_id":"1337"}')
+      fake_response_object = { "platform_ids" => ["bla_platform_987654321-123456789"],
+                               "updated_at" => 1000000000000,
+                               "created_at" => 1000000000000,
+                               "title" => "Game Name",
+                               "id" => "moew_app_123456789-987654321",
+                               "publish_id" => "1337" }
+
+      FakeWeb.allow_net_connect = false
+      FakeWeb.register_uri(:get, "https://#{Helpshift.configuration.api_key}@api.#{Helpshift.configuration.base_domain}/v1/#{Helpshift.configuration.customer_domain}/apps/#{app_publish_id}",
+                           :body => fake_response_object.to_json )
       found_app = Helpshift::App.find(app_publish_id)
 
       assert found_app.instance_of? Helpshift::App
--- test/unit/test_issue.rb
+++ test/unit/test_issue.rb
@@ -1,37 +1,17 @@
 require File.dirname(File.expand_path(__FILE__)) + '/../test_helper.rb'
 
 class Issue < Minitest::Test
-  context "issue" do
-    def setup
-      Helpshift.configure do |config|
-        config.customer_domain = "foobar"
-        config.api_key = "foobaz"
-      end
+  def setup
+    Helpshift.configure do |config|
+      config.api_key = "foobaz"
+      config.customer_domain = "foobar"
+      config.base_domain = 'helpshift.com'
     end
+  end
 
+  context "issue" do
     should "create new issue" do
-      FakeWeb.register_uri(:post, %r|https://api.#{Helpshift.configuration.base_domain}/|,
-                           :body => "empty")
 
-      issue = Helpshift::Issue.new
-      issue.email = "testuser@test.com"
-      issue.title = "test issue title"
-      issue.message_body = "test issue message body"
-      issue.app_id="test_app_123456-987654"
-      issue.platform_type = "android"
-      issue.tags = ["test_tag1", "test_tag2"]
-      issue.meta = { "meta-data" => "bla" }
-
-      issue.create
-
-      last_request = FakeWeb.last_request
-
-      assert_equal "email=testuser%40test.com&title=test%20issue%20title&" +
-                   "message-body=test%20issue%20message%20body&app-id=test_app_123456-987654&"+
-                   "platform-type=android&tags=%5B%22test_tag1%22%2C%22test_tag2%22%5D&"+
-                   "meta=%7B%22meta-data%22%3A%22bla%22%7D", last_request.body
-      assert_equal "POST", last_request.method
-      assert_equal "/v1/test/issues", last_request.path
     end
   end
 end
